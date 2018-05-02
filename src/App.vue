20f43b1f67bb4855b0fb36e59bd37e9f21fa17b9
lsy <272631750@qq.com>
2018-05-02 13:56:01 +0800

    更新

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <vue-tabbar-slide :options="options"></vue-tabbar-slide>
+  </div>
+</template>
+
+<script>
+import vueTabbarSlide from './lib/vueTabbarSlide'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      options: {
+        width: '50px'
+      }
+    }
+  },
+  components: {
+    vueTabbarSlide
+  }
+}
+</script>
+
+<style lang="scss">
+  * {
+    margin: 0;
+    padding: 0;
+  }
+</style>
---
+++ src/lib/vueTabbarSlide.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="tabbar-slide-wrapper">
+    <div class="tabbar-slide">
+      <div :style="slideOptions" class="tabbar-slide-list" ref="slide" v-for="(item, index) in slideArr" :key="index">
+        {{item}}
+      </div>
+    </div>
+    <div class="tabbar-slide-container"></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'vueTabbarSlide',
+    props: ['options'],
+    data () {
+      return {
+        slideArr: [1,2,3,4,5,6,7,8,9,10],
+        slideOptions: {
+          flex: `0 0 ${this.options.width}` || '0 0 50px'
+        }
+      }
+    },
+    mounted () {
+
+    }
+  }
+</script>
+
+<style>
+  .tabbar-slide-wrapper {
+    width: 100%;
+    background: #eee;
+    border-bottom: 1px solid red;
+    overflow: hidden;
+    font-family: 'Microsoft YaHei';
+    font-size: 14px;
+  }
+  .tabbar-slide {
+    display: flex;
+    align-items: center;
+  }
+  .tabbar-slide-list {
+    background: red;
+  }
+</style>
